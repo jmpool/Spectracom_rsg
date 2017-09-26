9709c47d00b1ccb4928c993b7d3236437ed38bfb
James P <jmp0040@auburn.edu>
2017-09-26 11:37:02 -0500

    More testing with bag files. Can now send ECEF rsg commands at 10 hz.

--- src/rsg_listener.cpp
+++ src/rsg_listener.cpp
@@ -4,6 +4,7 @@ float x_ECEF;
 float y_ECEF;
 float z_ECEF;
 
+
 void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
   x_ECEF = msg->pose.pose.position.x;
@@ -14,28 +15,31 @@ void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg)
 
 }
 
-// void chatterCallback(const std_msgs::String::ConstPtr& msg)
-// {
-//   ROS_INFO("I heard: [%s]", msg->data.c_str());
-// }
 
 int main(int argc, char **argv)
 {
 
-  // string scenarioName = "WoltoszStatic.scen";
-  string scenarioName = "blah";
+  string scenarioName = "WaltoszStatic.scen";
+  // string topicName = "/novatel_front/odom";
+  string topicName = "/Odometry";
+  string ip = "192.168.1.10";
+  int port = 5025;
 
-  if(argc < 3) {
-      std::cerr << "Usage: sim_controller_example <ip address> <port number>" << std::endl;
-      return 0;
-  }
+  // if(argc < 3) {
+  //     std::cerr << "Usage: rsg_listener <ip address> <port number>" << std::endl;
+  //     return 0;
+  // }
 
   spectracom::Spectracom_rsg * device = new spectracom::Spectracom_rsg;
   
-  std::string ip(argv[1]);
-  int port=5025;
-  istringstream(argv[2]) >> port;
+  // std::string ip(argv[1]);
+  // int port=5025;
+  // istringstream(argv[2]) >> port;
 
+  // ros::param::get("scenarioName", scenarioName);
+  // ros::param::get("topicName", topicName);
+  // ros::param::get("ip", ip);
+  // ros::param::get("port", port);
 
 
   if (!device->connect(ip,port)) {return -1;}
@@ -59,17 +63,14 @@ int main(int argc, char **argv)
   // Listen to rsg_talker
   ros::init(argc, argv, "listener");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("/Odometry", 1, chatterCallback);
-  // ros::Subscriber sub = n.subscribe("/novatel_front/odom", 1000, chatterCallback);
-  // ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-  ros::Rate r(1); // Run at 10 Hz
+  ros::Subscriber sub = n.subscribe(topicName, 1, chatterCallback);
+  ros::Rate r(10); // Run at 10 Hz
   while (ros::ok())
   {
-    
     ros::spinOnce();
-    cout << x_ECEF << endl;
+    // cout << x_ECEF << endl;
     device->sendRsgCommand(x_ECEF, y_ECEF, z_ECEF);
-    device->send("*OPC?");
+    // device->send("*OPC?");
     // device->commandWaitToContinue();
     device->queryPosition();
     r.sleep();
